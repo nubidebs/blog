d90b200792123acc38ae8b415d54cf73dbf6cfef
nubidebs <debora.crescenzo@truepill.com>
2022-06-09 15:09:13 +0100

    emitting/receiving events

--- comments/index.js
+++ comments/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
-app.use(cors());
-
 app.use(bodyParser.json());
+app.use(cors());
 
 const commentsByPostId = {};
 
@@ -14,17 +14,32 @@ app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
-  const postId = req.params.id;
+app.post('/posts/:id/comments', async (req, res) => {
   const commentId = randomBytes(4).toString('hex');
   const { content } = req.body;
 
-  const comments = commentsByPostId[postId] || [];
+  const comments = commentsByPostId[req.params.id] || [];
+
   comments.push({ id: commentId, content });
 
-  commentsByPostId[postId] = comments;
+  commentsByPostId[req.params.id] = comments;
+
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+    },
+  });
 
   res.status(201).send(comments);
+});
+
+app.post('/events', (req, res) => {
+  console.log('Event Received', req.body.type);
+
+  res.send({});
 });
 
 app.listen(4001, () => {
